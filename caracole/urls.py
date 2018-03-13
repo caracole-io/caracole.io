7bf36be397f2d1c8af5e3aae0214fe9f8b5d4521
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-13 23:59:47 +0100

    use all wagmail urls

--- caracole/urls.py
+++ caracole/urls.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 
-    path('', include(wagtail_urls)),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
 
     path('', TemplateView.as_view(template_name='base.html')),
 ]

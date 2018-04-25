42c491cdde9edfa2ae60e46297d72dfab14b2e53
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-26 00:02:50 +0200

    redirect vide-grenier

--- caracole/urls.py
+++ caracole/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
     path('videgrenier/', include('videgrenier.urls')),
+    path('vide-grenier/', RedirectView.as_view(url='/videgrenier/', permanent=True)),
 
     path('', TemplateView.as_view(template_name='base.html')),
 ]

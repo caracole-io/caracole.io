3775a1061cbb97183eafd64113d267a209c78310
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-11-13 00:57:22 +0100

    serve media in debug

--- caracole/urls.py
+++ caracole/urls.py
@@ -1,10 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView, TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,3 +23,6 @@ urlpatterns = [
     path('vide-grenier/', RedirectView.as_view(url='/videgrenier/', permanent=True)),
     path('', RedirectView.as_view(url='/pages/acceuil')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

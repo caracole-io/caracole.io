051b0a6c3d5c33c1cf4925b5cd352381d368df75
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-04-27 14:31:49 +0200

    remove JQuery.easing, fix embeded pdf

--- caracole/static/js/custom.js
+++ caracole/static/js/custom.js
@@ -28,6 +28,7 @@
 
 
   //jQuery to collapse the navbar on scroll
+  /*  Guilhem: doesn't work: TypeError: $(...).offset(...) is undefined
   $(window).scroll(function() {
     if ($(".navbar-default").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -36,5 +37,6 @@
     }
 
   });
+  */
 
 })(jQuery);

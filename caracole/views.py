b4bb0360637d05499750fd7e1903addaafe3a6a1
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-18 00:52:54 +0200

    wip

--- caracole/views.py
+++ caracole/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import messages
+from django.shortcuts import redirect
 from django.views.generic import CreateView
 
 from . import models
@@ -11,3 +13,16 @@ class CaramelView(CreateView):
     def form_valid(self, form):
         messages.success(self.request, 'Votre inscription au Caramel a bien été prise en compte')
         return super().form_valid(form)
+
+
+def mail_contact(request):
+    if request.method == 'POST':
+        print(request.POST)
+
+        print(f'To: {request.POST["form_dest"]}@{settings.PROJECT}.{settings.DOMAIN_NAME}')
+        print(f'Reply-To: {request.POST["form_name"]} <{request.POST["form_adress"]}>')
+        print('Subject: Message envoyé depuis le site')
+        print()
+        print(request.POST['form_message'])
+        messages.success(request, 'Votre message a bien été envoyé')
+    return redirect('/contacts/')
